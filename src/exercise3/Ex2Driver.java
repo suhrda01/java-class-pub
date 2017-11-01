b65889dd395101f7a5f72a27ba0ef77a8e0d717e
suhrda01 <suhrda01@ubuntu>
2017-10-31 21:29:35 -0500

    exercise 3 added

--- src/exercise3/Ex2Driver.java
+++ src/exercise3/Ex2Driver.java
@@ -1,94 +1,151 @@
 package exercise3;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.File;
+import java.io.OutputStream;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author yasiro01
  */
 public class Ex2Driver {
-  public static final String PATH = "data/";
-  /**
-   * main application function
-   * @param args 
-   */
-  public static void main(String[] args) {
-    int[][] matrix1 = null;
-    int[][] matrix2 = null;
-    int[][] result = null;
-    try {
-      matrix1 = readFile(PATH + "matrix1.txt");
-      matrix2 = readFile(PATH + "matrix2.txt");
-    } catch (FileNotFoundException ex) {
-      Logger.getLogger(Ex2Driver.class.getName()).log(Level.SEVERE, ex.toString(), ex);
+
+    public static final String PATH = "data/";
+
+    /**
+     * main application function
+     *
+     * @param args
+     * @throws java.io.FileNotFoundException 
+     */
+    public static void main(String[] args) throws IOException {
+        int[][] matrix1 = null;
+        int[][] matrix2 = null;
+        int[][] result = null;
+        try {
+            matrix1 = readFile(PATH + "matrix1.txt");
+            matrix2 = readFile(PATH + "matrix2.txt");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Ex2Driver.class.getName()).log(Level.SEVERE, ex.toString(), ex);
+        }
+        System.out.println("Matrix 1");
+        printMatrix(matrix1);
+        System.out.println("Matrix 2");
+        printMatrix(matrix2);
+        if (matrix1[0].length == matrix2.length) {
+            result = multiply(matrix1, matrix2);
+        } else {
+            Logger.getLogger(Ex2Driver.class.getName()).log(Level.SEVERE, "{0}", "Impossible to multiply matrices");
+            System.exit(0);
+        }
+        System.out.println("Result");
+        printMatrix(result);
+        try {
+            writeMatrix(result, PATH + "result.txt");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Ex2Driver.class.getName()).log(Level.SEVERE, ex.toString(), ex);
+        }
     }
-    System.out.println("Matrix 1");
-    printMatrix(matrix1);
-    System.out.println("Matrix 2");
-    printMatrix(matrix2);
-    if (matrix1[0].length == matrix2.length) {
-      result = multiply(matrix1, matrix2);
-    } else {
-      Logger.getLogger(Ex2Driver.class.getName()).log(Level.SEVERE, "{0}", "Impossible to multiply matrices");
-      System.exit(0);
+
+    /**
+     * Print a matrix
+     *
+     * @param matrix to print
+     */
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int item : row) {
+                System.out.printf("%5d", item);
+            }
+            System.out.printf("%n");
+        }
     }
-    System.out.println("Result");
-    printMatrix(result);
-    try {
-      writeMatrix(result, PATH + "result.txt");
-    } catch (FileNotFoundException ex) {
-      Logger.getLogger(Ex2Driver.class.getName()).log(Level.SEVERE, ex.toString(), ex);
+
+    /**
+     * Write a matrix to the file
+     *
+     * @param matrix
+     * @param filename
+     * @throws java.io.FileNotFoundException
+     */
+    public static void writeMatrix(int[][] matrix, String filename) throws FileNotFoundException {
+       PrintWriter outputFile = new PrintWriter(filename);
+       for (int[] row: matrix) {
+         for (int item: row) {
+           outputFile.printf("%5d", item);
+         }
+         outputFile.println("");
+       }
+       outputFile.close();
     }
-  }
-  /**
-   * Print a matrix
-   * @param matrix to print
-   */
-  public static void printMatrix(int[][] matrix) {
-    for (int[] row: matrix) {
-      for (int item: row) {
-        System.out.printf("%5d", item);
-      }
-      System.out.printf("%n");
+
+    /**
+     * Read a matrix from a file
+     *
+     * @param filename
+     * @return matrix read from a file
+     * @throws java.io.FileNotFoundException
+     */
+    public static int[][] readFile(String filename) throws FileNotFoundException, IOException {
+        BufferedReader inputFile = new BufferedReader(new FileReader(filename));
+        String line = inputFile.readLine();
+        String[] lineSplit = line.split(" ");
+        int rows = Integer.valueOf(lineSplit[0]);
+        int columns = Integer.valueOf(lineSplit[1]);
+        int[][] matrix = new int[rows][columns];
+        int lineCount = 0;
+        while ((line = inputFile.readLine()) != null) {
+            Scanner lineContent = new Scanner(line);
+            while (lineContent.hasNext()) {
+                lineContent.next();
+                String[] splitLine = line.split(" ");
+                for (int i =  0; i < splitLine.length; i++) {
+                    matrix[lineCount][i] = Integer.valueOf(splitLine[i]);
+                }
+            }
+        lineCount++;
     }
-  }
-  /**
-   * Write a matrix to the file
-   * @param matrix 
-   * @param filename 
-   * @throws java.io.FileNotFoundException 
-   */
-  public static void writeMatrix(int[][] matrix, String filename) throws FileNotFoundException {
-    throw new UnsupportedOperationException();
-  }
-  /**
-   * Read a matrix from a file
-   * @param filename
-   * @return matrix read from a file
-   * @throws java.io.FileNotFoundException
-   */
-  public static int[][] readFile(String filename) throws FileNotFoundException {
-    throw new UnsupportedOperationException();
-    /*
+    return matrix;
+    
+        /*
     10. Open the input file and create a Scanner object to read its content
     20. Read two values (rows and columns) from the first line, if possible
     30. Create a new 2-D array
     40. Read data from the file, one line at a time, using the Scanner object
     50. Split each line into individual tokens and put them into your array
     60. Return the array
-    */
-  }
-  /**
-   * Multiply two matrices
-   * @param matrix1
-   * @param matrix2
-   * @return the resulting matrix
-   */
-  public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
-    throw new UnsupportedOperationException();
-  }
+         */
+    }
+
+    /**
+     * Multiply two matrices
+     *
+     * @param matrix1
+     * @param matrix2
+     * @return the resulting matrix
+     */
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        int mat1 = matrix1.length;
+        int mat1b = matrix1[0].length;
+        int mat2 = matrix2.length;
+        int mat2b = matrix2[0].length;
+        int[][] matrix3 = new int[mat1][mat2b];
+        for (int i = 0; i < mat1; i++) {
+            for (int j = 0; j < mat2b; j++) {
+                for (int k = 0; k < mat1b; k++) {
+                    matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
+                }
+            }
+        }
+        return matrix3;
+    }
 }
