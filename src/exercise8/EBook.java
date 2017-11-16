0b153f0588d1eef7343c55e0a5f67fb6dc94f5e6
suhrda01 <suhrda01@ubuntu>
2017-11-16 13:53:44 -0600

    exercise 8

--- src/exercise8/EBook.java
+++ src/exercise8/EBook.java
@@ -7,13 +7,30 @@ package exercise8;
  */
 public class EBook extends Book {
   private final String format;
+
+  public EBook(String format, String author, int publishingYear, long id, String title) {
+      super(author, publishingYear, id, title, 1);
+      this.format = format;
+  }
   
+  @Override
+  public void checkout() {
+  }
+
+  @Override
+  public void checkin() {
+  }
   /**
    * Get the book format
    * @return the value of format
    */
   public String getFormat() {
     return format;
+  }
+
+  @Override
+  public String getInfo() {
+      return String.format("%s (%d). %s is available for %s", author, publishingYear, title, format);
   }
   
 }
